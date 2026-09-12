206586e7bb5a5577c9bd25df65b5b1a8e2abd58e
saifjb998-max <saifjb998@gmail.com>
2026-09-12 15:21:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ import sub from './sub.js';
 
 /* =========== Client ========== */
 const client = new Client({
-  phoneNumber: '20123456789', // Bot number
+  phoneNumber: '212727053545', // Bot number
   prefix: [".", "/", "!"],
   fromMe: false, 
   owners: [
   // Owner 1
-    { name: "VA", lid: "247579682029763@lid", jid: "212690125725@s.whatsapp.net" },
+    { name: "VA", lid: "247579682029763@lid", jid: "212727053545@s.whatsapp.net" },
   // Owner 2
-    { name: "emam", lid: "221307316789354@lid", jid: "212690125725@s.whatsapp.net" },
+    { name: "emam", lid: "221307316789354@lid", jid: "212727053545@s.whatsapp.net" },
   // Owner 3
-    { name: "Sukuna", jid: "212690125725@s.whatsapp.net", lid: "50414477168824@lid" },
+    { name: "Sukuna", jid: "21272053545@s.whatsapp.net", lid: "50414477168824@lid" },
   // Owner 4 
    { name: "عمورتي", jid: "212690125725@s.whatsapp.net", lid: "51664513925368@lid" }
   ],
